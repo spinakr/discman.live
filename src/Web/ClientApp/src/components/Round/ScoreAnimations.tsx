0867975873cd06d0816fdc477377cf19a896fdee
Anders Kofoed <anders.kfd@gmail.com>
2020-07-16 08:32:26 +0200

    Add more birdies

--- src/Web/ClientApp/src/components/Round/ScoreAnimations.tsx
+++ src/Web/ClientApp/src/components/Round/ScoreAnimations.tsx
@@ -17,6 +17,20 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux & {};
 
+const birdies = [
+  "birdie.png",
+  "birdie1.png",
+  "birdie2.png",
+  "birdie3.png",
+  "birdie4.png",
+];
+
+const getImage = (lastScore: RoundsStore.HoleScore) => {
+  if (lastScore?.relativeToPar === -1) {
+    return birdies[Math.floor(Math.random() * birdies.length)];
+  }
+};
+
 const ScoreAnimations = (props: Props) => {
   const { round, user } = props;
   const scores = round?.playerScores;
@@ -27,21 +41,29 @@ const ScoreAnimations = (props: Props) => {
       ?.scores.slice()
       .reverse();
 
-  const [animate, setAnimate] = useState(false);
+  const [animate, setAnimate] = useState("");
 
   const lastScore = userScores && userScores.find((s) => s.strokes !== 0);
 
   const isInitialMount = useRef(true);
+  const previousScoreValue = useRef(lastScore);
+
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
     } else {
-      if (lastScore?.relativeToPar === -1) {
-        setAnimate(true);
+      if (
+        lastScore &&
+        lastScore?.hole.number !== previousScoreValue.current?.hole.number
+      ) {
+        const imageName = getImage(lastScore);
+        if (!imageName) return;
+        setAnimate(imageName);
         setTimeout(() => {
-          setAnimate(false);
-        }, 7500);
+          setAnimate("");
+        }, 10000);
       }
+      previousScoreValue.current = lastScore;
     }
   }, [lastScore]);
 
@@ -49,7 +71,7 @@ const ScoreAnimations = (props: Props) => {
     <div>
       {animate && (
         <figure className="image flier">
-          <img src="birdie.png" alt="Birdie" />
+          <img src={animate} alt={animate} />
         </figure>
       )}
     </div>
