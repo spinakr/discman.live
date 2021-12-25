1f44394b6f02c8796982dca67871974efb40a48b
Anders Kofoed <anders.kfd@gmail.com>
2021-12-25 12:18:05 +0100

    Changes logs to serilog.postgres

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using NpgsqlTypes;
 using NServiceBus;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
 using Web.Infrastructure;
 using EnvironmentName = Microsoft.Extensions.Hosting.EnvironmentName;
 
@@ -39,15 +42,31 @@ namespace Web
                 .MinimumLevel.Override("Marten", LogEventLevel.Warning)
                 .WriteTo.Console()
                 .Enrich.WithProperty("ApplicationName", "discman.live")
-                .MinimumLevel.Information();
+                .MinimumLevel.Warning();
 
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var enableElk = Environment.GetEnvironmentVariable("ENABLE_ELK");
+            var enableElk = Environment.GetEnvironmentVariable("DOTNET_ENABLE_ELK");
             var isDevelopment = environment == Environments.Development;
             if (!isDevelopment && enableElk is not null && enableElk == "true")
             {
                 logConfig.WriteTo.Http("http://logstash:7000");
             }
+
+            string tableName = "logs";
+
+            var columnWriters = new Dictionary<string, ColumnWriterBase>
+            {
+                {"message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+                {"message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+                {"level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+                {"raise_date", new TimestampColumnWriter(NpgsqlDbType.Timestamp) },
+                {"exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+                {"properties", new LogEventSerializedColumnWriter(NpgsqlDbType.Jsonb) },
+                {"props_test", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+                {"machine_name", new SinglePropertyColumnWriter("MachineName", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") }
+            };
+
+            logConfig.WriteTo.PostgreSQL(Environment.GetEnvironmentVariable("DOTNET_POSTGRES_CON_STRING"), tableName, columnWriters, needAutoCreateTable: true);
 
             Log.Logger = logConfig.CreateLogger();
         }
